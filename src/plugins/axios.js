13918353c1c366ce11c0176777bed03401554496
‪Eihap karam‬‏ <eihapkaram9090@gmail.com>
2026-07-16 13:25:33 +0300

    Update axios base URL to ngrok endpoint

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'https://partridgelike-undelusively-gael.ngrok-free.dev/api/v1',
   timeout: 600000,
   headers: {
     'Content-Type': 'application/json',
